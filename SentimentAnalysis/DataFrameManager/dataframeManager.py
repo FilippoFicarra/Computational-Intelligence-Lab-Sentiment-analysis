d61154cb6120066fd100b471bb8eb688c7564d77
FilippoFicarra <82835577+FilippoFicarra@users.noreply.github.com>
2023-04-22 22:13:25 +0200

    Update: preprocessing pipeline

--- SentimentAnalysis/DataFrameManager/dataframeManager.py
+++ SentimentAnalysis/DataFrameManager/dataframeManager.py
@@ -1,17 +1,34 @@
 import pandas as pd
+from Preprocessing.textPreprocessor import TextPreprocessor
+from tqdm.auto import tqdm
 
+tqdm.pandas()
 
 class DataFrameManager:
     def __init__(self):
-        pass
-
-    def load_dataframe(self, filepath:str, encoding=None, names= None | list) -> pd.DataFrame:
-        df = pd.read_csv(filepath, encoding=encoding, names=names)
+        self.preprocessor = TextPreprocessor()
+    
+    def preprocess_text(self, text):
+        return self.preprocessor.preprocess_text(text)
+    
+    def preprocess_df(self, df):
         decode_map = {0: "NEGATIVE", 2: "NEUTRAL", 4: "POSITIVE"}
+
         def decode_sentiment(label):
             return decode_map[int(label)]
-        df.target = df.target.apply(lambda x: decode_sentiment(x))
+
+        df.target = df.target.progress_apply(decode_sentiment)
+        df.text = df.text.progress_apply(self.preprocess_text)
+
         return df
-    
+
+
+    def load_dataframe(self, filepath:str, encoding=None, names=None, preprocess=True) -> pd.DataFrame:
+        df = pd.read_csv(filepath, encoding=encoding, names=names)
+        if preprocess:
+            print("Preprocessing the text...")
+            df = self.preprocess_df(df)
+        return df
+
     def export_dataframe(self, df : pd.DataFrame, filepath : str) -> None:
         df.to_csv(filepath, index=False)
--- SentimentAnalysis/Preprocessing/textPreprocessor.py
+++ SentimentAnalysis/Preprocessing/textPreprocessor.py
@@ -2,8 +2,13 @@ from nltk.corpus import stopwords
 import language_tool_python
 import contractions
 import string
+import re
 
 class TextPreprocessor:
+    """
+    This class is used to preprocess the text of the tweets.
+    It corrects the spelling, removes the punctuation, stopwords, links and expands the contractions.
+    """
     def __init__(self):
         self.stop_words = set(stopwords.words('english'))
        
@@ -16,6 +21,10 @@ class TextPreprocessor:
         self.tool = language_tool_python.LanguageTool('en-US')
 
     def preprocess_text(self, text : str) -> str:
+
+        ## Remove links
+        # TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S"
+        # text = re.sub(TEXT_CLEANING_RE, ' ', str(text).lower()).strip()
 
         ## Correct spelling
         text = self.tool.correct(text).lower()       
@@ -30,13 +39,13 @@ class TextPreprocessor:
         text = contractions.fix(text)
 
         # It could be useful to lemmatize and use part of speech tagging in order to better compare and translate the words in the vector space
-        
 
         return text
 
 
 if __name__ == '__main__':
     text = "I'm a students. I'm learnig NLP."
+    text = "I'm a students. I'm learnig NLP. https://www.google.com"
     text_preprocessor = TextPreprocessor()
     preprocessed_text = text_preprocessor.preprocess_text(text)
     print(preprocessed_text)
