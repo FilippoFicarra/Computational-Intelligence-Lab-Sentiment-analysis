cf37f86a59489fe57783ab9f05c5d03b90e20af2
FilippoFicarra <82835577+FilippoFicarra@users.noreply.github.com>
2023-04-23 03:43:25 +0200

    Feature: NGrams from embeddings

---
+++ SentimentAnalysis/NGrams/ngrams.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class NGrams:
+    """
+    This class is used to generate n-grams from a sequence of word embeddings.
+    Functions:
+        - generate_ngrams(embeddings : list, n : int) -> list
+    """
+    def __init__(self):
+        pass
+
+    def generate_ngrams(self, embeddings : list, n : int) -> list:
+        """
+        Generates n-grams from a sequence of word embeddings.
+
+        Args:
+        embeddings: a list or array of word embeddings
+        n: the length of the n-grams
+
+        Returns:
+        a list of n-grams
+        """
+        ngrams = []
+        for i in range(len(embeddings)-n+1):
+            ngram = np.concatenate([embeddings[j] for j in range(i,i+n)])
+            ngrams.append(ngram)
+        return ngrams
