c67136f9ebfc3416047be3bec89082047ed84e23
FilippoFicarra <82835577+FilippoFicarra@users.noreply.github.com>
2023-05-18 21:08:57 +0200

    update: embedder with batches

--- SentimentAnalysis/Embedder/embedder.py
+++ SentimentAnalysis/Embedder/embedder.py
@@ -1,23 +1,27 @@
-from DataFrameManager.dataframeManager import DataFrameManager
-import os
+from functools import partial
+import numpy as np
 import torch
 from transformers import BertModel, BertTokenizer, RobertaTokenizer, RobertaModel
-import numpy as np
 from tqdm import tqdm
-from NGrams.ngrams import NGrams
-import environ
-from  config import Config
-
+from typing import List
+from common import utils
+import multiprocessing
+import pandas as pd
+from tqdm.auto import tqdm
+# from NGrams.ngrams import NGrams
+# import environ
+# from  config import Config
 
 class Embedder:
     """
     This class is used to get the embeddings of a text using the BERT model.
 
     Methods:
-        - get_embeddings(text : str) -> torch.Tensor
+        - get_embeddings(text : list[str]) -> torch.Tensor
     """
     def __init__(self, model_name : str = 'bert'):
         self.model_name = model_name
+        self.batch_size = 32
         if self.model_name == 'bert':
             self._model_name = 'bert-large-uncased'
             self.tokenizer = BertTokenizer.from_pretrained(self._model_name)
@@ -28,91 +32,40 @@ class Embedder:
             self.model = RobertaModel.from_pretrained(self._model_name)
         self.model.eval()
 
-    def get_embeddings(self, text : str) -> torch.Tensor:
+
+
+    def get_embeddings(self, texts: List[str]) -> torch.Tensor:
         """
-        This function gets the embeddings of a text using the  'model_name' model.
+        This function gets the embeddings of a list of texts using the 'model_name' model.
 
         Args:
-            - text : str
-            - model_name : str
+            - texts : List[str]
         Returns:
             - embeddings : torch.Tensor
         """
-  
-            
-        # Tokenize the text
-        inputs = self.tokenizer.encode_plus(text, add_special_tokens=True, return_tensors='pt')
-        input_ids = inputs['input_ids']
-        attention_masks = inputs['attention_mask']
-        if self.model_name == 'bert':
-            token_type_ids = inputs['token_type_ids']
 
         # Obtain the output embeddings
-        with torch.no_grad():
-            if self.model_name == 'bert':
-                outputs = self.model(input_ids, attention_mask=attention_masks, token_type_ids=token_type_ids)
-            elif self.model_name == 'roberta':
-                outputs = self.model(input_ids, attention_mask=attention_masks)
-            embeddings = outputs[0][0]  # take the first element of the batch and the last hidden layer
+        embeddings = []
+        with torch.no_grad(), tqdm(total=len(texts), desc='Embedding Progress') as pbar:
+            for i in range(0, len(texts), self.batch_size):
+                
+                # Tokenize the input texts
+                batch_inputs = self.tokenizer.batch_encode_plus(texts[i:i + self.batch_size], add_special_tokens=True, return_tensors='pt', padding=True, truncation=True)
+                batch_input_ids = batch_inputs['input_ids']
+                batch_attention_masks = batch_inputs['attention_mask']
+
+                if self.model_name == 'bert':
+                    batch_token_type_ids = batch_inputs[i:i + self.batch_size]
+
+                if self.model_name == 'bert':
+                    outputs = self.model(batch_input_ids, attention_mask=batch_attention_masks, token_type_ids=batch_token_type_ids)
+                elif self.model_name == 'roberta':
+                    outputs = self.model(batch_input_ids, attention_mask=batch_attention_masks)
+                batch_embeddings = outputs.last_hidden_state[:, 0, :]  # Extract the embeddings for the [CLS] token of each input
+
+                embeddings.append(batch_embeddings)
+                pbar.update(batch_input_ids.shape[0])
+
+        embeddings = torch.cat(embeddings, dim=0)  # Concatenate embeddings from all batches
         return embeddings
-
-
-## Example usage
-# if __name__ == '__main__':
-#     config = environ.to_config(Config)
-    
-#     directory_path = 'SentimentAnalysis/Data'
-#     filename = 'preprocessed.csv'
-#     file_path = os.path.join(directory_path, filename)
-#     data_frame_manager = DataFrameManager(config.num_cpus)
-#     N = 4
-
-#     DATASET_COLUMNS = ["target", "ids", "date", "flag", "user", "text"]
-#     DATASET_ENCODING = "ISO-8859-1"
-#     if os.path.exists(file_path):
-#         print("File already exists, loading it...")
-#         df = data_frame_manager.load_dataframe(filepath="SentimentAnalysis/Data/preprocessed.csv", encoding=DATASET_ENCODING, names=DATASET_COLUMNS, preprocess=False)
-#         print("File loaded.")
-#         print(len(df))
-#     else:
-#         print("File does not exist, loading the original dataset and preprocessing it...")
-#         df = data_frame_manager.load_dataframe(filepath="SentimentAnalysis/Data/training.1600000.processed.noemoticon.csv", encoding=DATASET_ENCODING, names=DATASET_COLUMNS).dropna(subset=['text'])
-#         data_frame_manager.export_dataframe(df, filepath="SentimentAnalysis/Data/preprocessed.csv")
-#         print("Preprocessing done and saved to CSV file.")
-    
-#     train_df, test_df = data_frame_manager.split(df = df)
-
-#     embedder = Embedder()
-
-#     # Get the embeddings for the test set
-#     if not os.path.exists('SentimentAnalysis/Data/test_embeddings.npy'):
-#         test_embeddings = test_df.text.progress_apply(embedder.get_embeddings)
-#         print("File does not exist, saving the embeddings...")
-#         with open('SentimentAnalysis/Data/test_embeddings.npy', 'xb') as f:
-#             np.save(f, test_embeddings.to_numpy())
-#     else:
-#         print("File already exists, loading it...")
-#         test_embeddings = np.load('SentimentAnalysis/Data/test_embeddings.npy', allow_pickle=True)
-#         print(f"Loaded {test_embeddings.shape} array from 'SentimentAnalysis/Data/test_embeddings.npy'")
-
-#     # Get the embeddings for the train set
-#     if not os.path.exists('SentimentAnalysis/Data/train_embeddings.npy'):
-#         train_embeddings = train_df.text.progress_apply(embedder.get_embeddings)
-#         print("File does not exist, saving the embeddings...")
-#         with open('SentimentAnalysis/Data/train_embeddings.npy', 'xb') as f:
-#             np.save(f, train_embeddings.to_numpy())
-#     else:
-#         print("File already exists, loading it...")
-#         train_embeddings = np.load('SentimentAnalysis/Data/train_embeddings.npy', allow_pickle=True)
-#         print(f"Loaded {train_embeddings.shape} array from 'SentimentAnalysis/Data/train_embeddings.npy'")
-
-#     # n_grams = NGrams()
-    
-#     # print("Getting the n-grams for the test set...")
-#     # test_ngrams = [n_grams.generate_ngrams(embeddings, N) for embeddings in tqdm(test_embeddings, desc='Generating n-grams')]
-#     # print("Getting the n-grams for the train set...")
-#     # train_ngrams = [n_grams.generate_ngrams(embeddings, N) for embeddings in tqdm(train_embeddings, desc='Generating n-grams')]
-
-    
-    
 
