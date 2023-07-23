bfd94d4af75d343aba620f9489ba284c75d93957
Paul Doucet <homedoucetpaul@gmail.com>
2023-07-23 21:40:43 +0200

    clean repo

--- SentimentAnalysis/config.py
+++
@@ -1,9 +0,0 @@
-import multiprocessing
-import environ
-
-@environ.config(prefix='')
-class Config:
-    """The common configuration for the project"""
-    
-    num_cpus = environ.var(default=multiprocessing.cpu_count())
-    
