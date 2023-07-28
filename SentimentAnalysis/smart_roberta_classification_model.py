f912964aa19f902ead045ea4714f08d009c69053
marcoCalzavara <100380019+marcoCalzavara@users.noreply.github.com>
2023-07-28 23:12:49 +0200

    Fixed bug with smart

--- SentimentAnalysis/smart_roberta_classification_model.py
+++ SentimentAnalysis/smart_roberta_classification_model.py
@@ -1,12 +1,27 @@
-import torch.nn as nn
+import torch
 
 
-class SMARTRobertaClassificationModel(nn.Module):
+class SMARTRobertaClassificationModel(torch.nn.Module):
 
     def __init__(self, model, weight=0.02):
         super().__init__()
         self.model = model
         self.weight = weight
+        self.epoch = 0
 
     def forward(self, input_ids, attention_mask):
         return self.model(inputs_ids=input_ids, attention_mask=attention_mask)
+
+    def update_epoch(self, epoch):
+        self.epoch = epoch
+
+    def model_representation(self):
+        return {
+            'epoch': self.epoch,
+            'model_state_dict': self.state_dict()
+        }
+
+    def load_model(self, file_path):
+        state = torch.load(file_path)
+        self.epoch = state['epoch']
+        self.load_state_dict(state['model_state_dict'])
