1cc0d539be7c43b6224a88cde88ec537f5369874
marcoCalzavara <100380019+marcoCalzavara@users.noreply.github.com>
2023-07-29 00:54:12 +0200

    Fixed SMART model

--- SentimentAnalysis/smart_roberta_classification_model.py
+++ SentimentAnalysis/smart_roberta_classification_model.py
@@ -17,15 +17,14 @@ class SMARTRobertaClassificationModel(nn.Module):
         embed = self.model.base_model.embeddings(input_ids)
 
         def eval_fn(embed):
-            outputs = self.model.base_model(inputs_embeds=embed, attention_mask=attention_mask)
-            pooled = outputs.last_hidden_state
-            logits = self.model.classifier(pooled)
+            encoder_out = self.model.base_model.encoder(embed, attention_mask=attention_mask)
+            pooler_out = self.model.base_model.pooler(encoder_out[0])
+            logits = self.model.classifier(pooler_out)
             return logits
 
         smart_loss_fn = SMARTLoss(eval_fn=eval_fn, loss_fn=kl_loss, loss_last_fn=sym_kl_loss)
         state = eval_fn(embed)
-        gt_state = nn.functional.one_hot(labels, num_classes=2)
-        loss = nn.functional.cross_entropy(state, gt_state)
+        loss = nn.functional.cross_entropy(state, labels)
         loss += self.weight * smart_loss_fn(embed, state)
 
         return state, loss
@@ -34,9 +33,9 @@ class SMARTRobertaClassificationModel(nn.Module):
         embed = self.model.base_model.embeddings(input_ids)
 
         def eval_fn(embed):
-            outputs = self.model.base_model(inputs_embeds=embed, attention_mask=attention_mask)
-            pooled = outputs.last_hidden_state
-            logits = self.model.classifier(pooled)
+            encoder_out = self.model.base_model.encoder(embed, attention_mask=attention_mask)
+            pooler_out = self.model.base_model.pooler(encoder_out[0])
+            logits = self.model.classifier(pooler_out)
             return logits
 
         state = eval_fn(embed)
